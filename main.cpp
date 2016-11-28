c4efe25c4eabdfbdefd15477a2d935a805c80f10
Pedro Santos <pedro@codurance.com>
2016-11-28 11:41:38 +0000

    Display ball on top left corner

--- main.cpp
+++ main.cpp
@@ -55,14 +55,14 @@ namespace Arkanoid
 
     struct Render : Node
     {
-        std::unique_ptr<Dispaly> display;
-        std::unique_ptr<Position> position;
+        std::shared_ptr<Dispaly> display;
+        std::shared_ptr<Position> position;
     };
 
     struct Move : Node
     {
-        std::unique_ptr<Position> position;
-        std::unique_ptr<Velocity> velocity;
+        std::shared_ptr<Position> position;
+        std::shared_ptr<Velocity> velocity;
     };
 
     struct System
@@ -71,19 +71,19 @@ namespace Arkanoid
 
     struct Mover : System
     {
-        std::unique_ptr<Move> move;
+        std::shared_ptr<Move> move;
     };
 
     struct Renderer : System
     {
-        std::unique_ptr<Render> render;
+        std::shared_ptr<Render> render;
 
         Renderer(sf::RenderWindow& window)
         {
             window.setFramerateLimit(60);
         }
 
-        void Update(sf::RenderWindow& window)
+        void Update(sf::RenderWindow& window) const
         {
             window.clear(sf::Color::Black);
             auto shape = render->display->Shape();
@@ -99,19 +99,14 @@ namespace Arkanoid
 
     struct Ball : Entity
     {
-        std::unique_ptr<Circle> circle;
-        std::unique_ptr<Position> position;
-        std::unique_ptr<Velocity> velocity;
+        std::shared_ptr<Circle> circle;
+        std::shared_ptr<Position> position;
+        std::shared_ptr<Velocity> velocity;
 
         Ball()
         {
             sf::Vector2f ballVelocity{0, 0};
-            sf::Vector2f ballPosition{windowWidth / 2, windowHeight / 2};
-
-            circle = std::make_unique<Circle>(ballRadius, sf::Color::Red);
-            position = std::make_unique<Position>(ballPosition);
-
-            velocity = std::make_unique<Velocity>(ballVelocity);
+            velocity = std::make_shared<Velocity>(ballVelocity);
         }
     };
 }
@@ -119,9 +114,21 @@ namespace Arkanoid
 int main()
 {
     sf::RenderWindow window{{windowWidth, windowHeight}, "Arkanoid"};
+	sf::Vector2f ballPosition{ windowWidth / 2, windowHeight / 2 };
 
-    Arkanoid::Ball ball = Arkanoid::Ball();
-    Arkanoid::Renderer renderer = Arkanoid::Renderer(window);
+	auto circle = std::make_shared<Arkanoid::Circle>(ballRadius, sf::Color::Red);
+	auto position = std::make_shared<Arkanoid::Position>(ballPosition);
+
+	auto render = std::make_shared<Arkanoid::Render>();
+	render->display = circle;
+	render->position = position;
+
+	auto renderer = Arkanoid::Renderer(window);
+	renderer.render = render;
+
+	auto ball = Arkanoid::Ball();
+	ball.position = position;
+	ball.circle = circle;
 
     while(window.isOpen())
     {
